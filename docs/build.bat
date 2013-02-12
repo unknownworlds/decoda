0a74f8d1c78b7c1f068a76328713d9faadee1b70
Max McGuire <max_mcguire@yahoo.com>
2013-02-12 12:15:32 -0800

    Adjusted documentation building script for the default install directory on 64-bit Windows

--- docs/build.bat
+++ docs/build.bat
@@ -1,5 +1,5 @@
 copy .\images\*.* .\output
 tex2rtf decoda.tex output/Decoda -html -twice -macros tex2rtf.ini
-"C:\Program Files\HTML Help Workshop\hhc" output/Decoda.hhp
+"C:\Program Files (x86)\HTML Help Workshop\hhc" output/Decoda.hhp
 pause
 start output/Decoda.chm
